a9ff715d097424e7aec7c0717f2e1c49263cfc0b
Rekguard <ignieire@gmail.com>
2014-02-11 20:20:05 +0000

    Updated Knight class's legal move display method to prevent ArrayOutOfBounds errors from appearing.

--- JChessGame/src/Pieces/ChessPieces/Knight.java
+++ JChessGame/src/Pieces/ChessPieces/Knight.java
@@ -35,52 +35,76 @@ public class Knight extends ChessPiece {
 		int chaArrayPos = Character.getNumericValue(characterPosition) - 10;
 		int numArrayPos = numberPosition - 1;
 		
+		
+		int farLD = 0;		// Far Left side and Down have the same value
+		int clsLD = 1;		// Close Left side and Down have the same value
+		int clsRU = 6;		// Close Right side and Up have the same value
+		int farRU = 7;		// Far Right side and Up have the same value
+		
+		
+		
 			/* NNW */
-		if(bpShow[chaArrayPos-1][numArrayPos+2].isOccupied() == false){
-			moveOutline[0].setOutlinePositon(bpShow[chaArrayPos-1][numArrayPos+2].getLocation());
-			moveOutline[0].drawAtLocation();
+		if(chaArrayPos > farLD && numArrayPos < clsRU){
+			if(bpShow[chaArrayPos-1][numArrayPos+2].isOccupied() == false){
+				moveOutline[0].setOutlinePositon(bpShow[chaArrayPos-1][numArrayPos+2].getLocation());
+				moveOutline[0].drawAtLocation();
+			}
 		}
 		
 			/* NWW */
-		if(bpShow[chaArrayPos-2][numArrayPos+1].isOccupied() == false){
-			moveOutline[0].setOutlinePositon(bpShow[chaArrayPos-2][numArrayPos+1].getLocation());
-			moveOutline[0].drawAtLocation();
+		if(chaArrayPos > clsLD && numArrayPos < farRU){
+			if(bpShow[chaArrayPos-2][numArrayPos+1].isOccupied() == false){
+				moveOutline[0].setOutlinePositon(bpShow[chaArrayPos-2][numArrayPos+1].getLocation());
+				moveOutline[0].drawAtLocation();
+			}
 		}
 		
 			/* SWW */
-		if(bpShow[chaArrayPos][numArrayPos].isOccupied()){
-			moveOutline[0].setOutlinePositon(bpShow[chaArrayPos][numArrayPos].getLocation());
-			moveOutline[0].drawAtLocation();
+		if(chaArrayPos > clsLD && numArrayPos > farLD){
+			if(bpShow[chaArrayPos-2][numArrayPos-1].isOccupied() == false){
+				moveOutline[0].setOutlinePositon(bpShow[chaArrayPos-2][numArrayPos-1].getLocation());
+				moveOutline[0].drawAtLocation();
+			}
 		}
 		
 			/* SSW */
-		if(bpShow[chaArrayPos][numArrayPos].isOccupied()){
-			moveOutline[0].setOutlinePositon(bpShow[chaArrayPos][numArrayPos].getLocation());
-			moveOutline[0].drawAtLocation();
+		if(chaArrayPos > farLD && numArrayPos > clsLD){
+			if(bpShow[chaArrayPos-1][numArrayPos-2].isOccupied() == false){
+				moveOutline[0].setOutlinePositon(bpShow[chaArrayPos-1][numArrayPos-2].getLocation());
+				moveOutline[0].drawAtLocation();
+			}
 		}
 		
 			/* SSE */
-		if(bpShow[chaArrayPos][numArrayPos].isOccupied()){
-			moveOutline[0].setOutlinePositon(bpShow[chaArrayPos][numArrayPos].getLocation());
-			moveOutline[0].drawAtLocation();
+		if(chaArrayPos < farRU && numArrayPos > clsLD){	
+			if(bpShow[chaArrayPos+1][numArrayPos-2].isOccupied() == false){
+				moveOutline[0].setOutlinePositon(bpShow[chaArrayPos+1][numArrayPos-2].getLocation());
+				moveOutline[0].drawAtLocation();
+			}
 		}
 		
 			/* SEE */
-		if(bpShow[chaArrayPos][numArrayPos].isOccupied()){
-			moveOutline[0].setOutlinePositon(bpShow[chaArrayPos][numArrayPos].getLocation());
-			moveOutline[0].drawAtLocation();
+		if(chaArrayPos < clsRU && numArrayPos > farLD){
+			if(bpShow[chaArrayPos+2][numArrayPos-1].isOccupied() == false){
+				moveOutline[0].setOutlinePositon(bpShow[chaArrayPos+2][numArrayPos-1].getLocation());
+				moveOutline[0].drawAtLocation();
+			}
 		}
 		
 			/* NEE */
-		if(bpShow[chaArrayPos][numArrayPos].isOccupied()){
-			moveOutline[0].setOutlinePositon(bpShow[chaArrayPos][numArrayPos].getLocation());
-			moveOutline[0].drawAtLocation();
+		if( chaArrayPos < clsRU && numArrayPos < farRU){
+			if(bpShow[chaArrayPos+2][numArrayPos+1].isOccupied() == false){
+				moveOutline[0].setOutlinePositon(bpShow[chaArrayPos+2][numArrayPos+1].getLocation());
+				moveOutline[0].drawAtLocation();
+			}
 		}
 		
 			/* NNE */
-		if(bpShow[chaArrayPos][numArrayPos].isOccupied()){
-			moveOutline[0].setOutlinePositon(bpShow[chaArrayPos][numArrayPos].getLocation());
-			moveOutline[0].drawAtLocation();
+		if( chaArrayPos < farRU && numArrayPos < clsRU){
+			if(bpShow[chaArrayPos+1][numArrayPos+2].isOccupied() == false){
+				moveOutline[0].setOutlinePositon(bpShow[chaArrayPos+1][numArrayPos+2].getLocation());
+				moveOutline[0].drawAtLocation();
+			}
 		}
 		
 	}
